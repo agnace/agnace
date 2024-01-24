78998f0aca85bfe9b063e89a2f648ea639c2d15e
agnace <Agnace12elias@gmail.com>
2024-01-24 22:52:31 +0100

    Create funcao.js

---
+++ funcao.js
@@ -0,0 +1,12 @@
+function dias (dia){
+    return dia;
+}
+function ano (ano){
+    return ano;
+}
+function mes (mes){
+    return mes;
+}
+console.log(dias(12))
+console.log(ano(2000))
+console.log(mes(8))
